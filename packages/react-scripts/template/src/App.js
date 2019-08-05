239055b70f3f6f868c4d89248f46c76a9e52d27a
Rahulgarg30591 <rgarg@celestialsys.com>
2019-08-05 12:06:54 +0530

    EWC-24 | React Script is updated.

--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -4,22 +4,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ext-panel
+      title="EWC Panel in React"
+      bodyPadding="20px"
+    >
+      <h1>I am the ExtWebComponents Panel</h1>
+    </ext-panel>
   );
 }
 
